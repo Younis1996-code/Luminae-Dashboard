1ee5e27224a1fb3d984b1df725c9eac7912e9df4
hasanWhatfa <wtfyh05@gmail.com>
2025-11-03 03:00:30 +0300

    firebase Config

---
+++ app/utils/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:process.env.FIRE_BASE_API ,
+  authDomain: process.env.FIRE_BASE_AUTH_DOMAIN,
+  databaseURL:process.env.FIRE_BASE_DATA_BASE_URL,
+  projectId: "luminae-ca224",
+  storageBucket: "luminae-ca224.firebasestorage.app",
+  messagingSenderId: "1048878162907",
+  appId:process.env.FIRE_BASE_APP_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
